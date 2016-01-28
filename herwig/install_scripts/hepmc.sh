0dad0dd06ef061b29d2a3abff20632f18966b488
Lukas Heinrich <lukas.heinrich@gmail.com>
2016-01-28 01:36:28 -0500

    use Herwig bootstrap script

--- herwig/install_scripts/hepmc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-HEPMC_VERSION=$1
-wget http://lcgapp.cern.ch/project/simu/HepMC/download/HepMC-$HEPMC_VERSION.tar.gz
-tar -xzvf HepMC-$HEPMC_VERSION.tar.gz
-cd HepMC-$HEPMC_VERSION
-./configure --prefix /usr/local --with-momentum=GEV --with-length=MM
-make -j4
-make install 
--- herwig/install_scripts/python27.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-curl https://www.python.org/ftp/python/2.7.8/Python-2.7.8.tgz | tar -xzvf -
-cd Python-2.7.8
-./configure --prefix /usr/local  --enable-shared
-make -j4
-make install
